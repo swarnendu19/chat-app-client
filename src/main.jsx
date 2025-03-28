9259472b2ceb077653bcf531ede8602d4c0d31a7
Swarnendu <swarnendumaity633@gmail.com>
2025-03-28 23:51:14 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import ThemeContextWrapper from "./context/ThemeContextWrapper.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <SpeedInsights />
+    <BrowserRouter>
+      <ThemeContextWrapper>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </ThemeContextWrapper>
+    </BrowserRouter>
+  </React.StrictMode>
+);
